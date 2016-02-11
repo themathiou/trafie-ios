e355d174af5c48fa5a8fefe457583dcecd5baed1
mathiou <tmathioudakis@gmail.com>
2016-02-11 20:51:38 +0200

    Adding Utils.swift. Split items from Common.swift

---
+++ trafie/Utils.swift
@@ -0,0 +1,254 @@
+//
+//  Utils.swift
+//  trafie
+//
+//  Created by mathiou on 11/02/16.
+//  Copyright © 2016 Mathioudakis Theodore. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+import UIKit
+import PromiseKit
+
+// MARK:- Functions
+// MARK: App Initialization
+func validateInitValuesOfProfile() {
+    if NSUserDefaults.standardUserDefaults().objectForKey("token") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "token")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("userId") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "userId")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("firstname") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "firstname")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("lastname") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "lastname")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("about") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "about")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("mainDiscipline") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "mainDiscipline")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("isPrivate") == nil {
+        NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isPrivate")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("isMale") == nil {
+        NSUserDefaults.standardUserDefaults().setObject(true, forKey: "isMale")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("birthday") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "birthday")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("country") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "country")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("email") == nil {
+        NSUserDefaults.standardUserDefaults().setObject("", forKey: "email")
+    }
+    
+    if NSUserDefaults.standardUserDefaults().objectForKey("isValid") == nil {
+        NSUserDefaults.standardUserDefaults().setBool(false, forKey: "isValid")
+    }
+    log("Completed")
+}
+
+func resetValuesOfProfile() {
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "token")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "userId")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "firstname")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "lastname")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "about")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "mainDiscipline")
+    NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isPrivate")
+    NSUserDefaults.standardUserDefaults().setObject(true, forKey: "isMale")
+    NSUserDefaults.standardUserDefaults().setObject(false, forKey: "isValid")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "birthday")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "country")
+    NSUserDefaults.standardUserDefaults().setObject("", forKey: "email")
+    log("Completed")
+}
+
+// MARK:- Calculation Functions
+func convertPerformanceToReadable(performance: String, discipline: String) -> String {
+    var readable : String = ""
+    let performanceInt : Int = Int(performance)!
+    
+    //Time
+    if disciplinesTime.contains(discipline) {
+        let centisecs = (performanceInt % 100)
+        let secs = ((performanceInt) % 6000) / 100
+        let mins = (performanceInt % 360000) / 6000
+        let hours = (performanceInt - secs - mins - centisecs) / 360000
+        
+        //fill with zeros if needed
+        var  minsPart : String = "00:"
+        var  secsPart : String = "00:"
+        if mins != 0 {
+            minsPart = mins < 10 ? "0\(String(mins)):" : "\(String(mins)):"
+        }
+        if secs != 0 {
+            secsPart = secs < 10 ? "0\(String(secs))." : "\(String(secs))."
+        }
+        
+        readable  = secsPart + "\(String(centisecs))"
+        
+        if hours != 0 {
+            readable = "\(String(hours)):" + minsPart + readable
+        } else {
+            if mins != 0 {
+                readable = minsPart + readable
+            }
+        }
+        
+        return readable
+        // Distance
+    } else if disciplinesDistance.contains(discipline) {
+        let centimeters = (performanceInt % 10000) / 100
+        let meters = (performanceInt - centimeters) / 10000
+        
+        readable = centimeters < 10 ? "\(String(meters)).0\(String(centimeters))" : "\(String(meters)).\(String(centimeters))"
+        
+        return readable
+        // Points
+    } else if disciplinesPoints.contains(discipline){
+        let hundreds = (performanceInt % 1000)
+        let thousand = (performanceInt - hundreds) / 1000
+        var readable : String = ""
+        var zerosForHundred = ""
+        
+        if hundreds < 100 && hundreds > 10 {
+            zerosForHundred = "0"
+        } else if hundreds < 10 {
+            zerosForHundred = "00"
+        }
+        
+        readable = "\(String(thousand)).\(zerosForHundred+String(hundreds))" //10.045
+        return readable
+    }
+    
+    return performance
+}
+
+
+// MARK: Pickers and Ranges
+func createIntRangeArray(from: Int, to: Int, addZeros: Bool?=true) -> [String] {
+    var array: [String] = []
+    for index in from..<to {
+        // add zero in front of one-digit numbers
+        let value : String = ((addZeros! == true)  && (index < 10)) ? String(format: "%02d", index) : String(index)
+        array.append(value)
+    }
+    return array
+}
+
+func getPerformanceLimitationsPerDiscipline(discipline: String) -> [[String]] {
+    switch discipline {
+        //distance disciplines
+    case "high_jump":
+        return [createIntRangeArray(1, to: 3, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "long_jump":
+        return [createIntRangeArray(0, to: 10, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "triple_jump":
+        return [createIntRangeArray(0, to: 19, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "pole_vault":
+        return [createIntRangeArray(0, to: 7, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "shot_put":
+        return [createIntRangeArray(0, to: 24, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "discus":
+        return [createIntRangeArray(0, to: 75, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "hammer":
+        return [createIntRangeArray(0, to: 88, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+    case "javelin":
+        return [createIntRangeArray(0, to: 99, addZeros: false), ["."], createIntRangeArray(0, to: 100)]
+        //time disciplines
+    case "60m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "100m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "200m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "400m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "800m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "1500m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "3000m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "5000m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "10000m":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "60m_hurdles":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "100m_hurdles":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "110m_hurdles":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "400m_hurdles":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "3000m_steeplechase":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "4x100m_relay":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "4x400m_relay":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "half_marathon":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "marathon":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "20km_race_walk":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "50km_race_walk":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+    case "cross_country_running":
+        return [createIntRangeArray(0, to: 10), [":"], createIntRangeArray(0, to: 60), [":"], createIntRangeArray(0, to: 60), ["."], createIntRangeArray(0, to: 100)]
+        //points disciplines
+    case "pentathlon":
+        return [createIntRangeArray(0, to: 7, addZeros: false), ["."], createIntRangeArray(0, to: 10, addZeros: false), createIntRangeArray(0, to: 10, addZeros: false), createIntRangeArray(0, to: 10, addZeros: false)]
+    case "heptathlon":
+        return [createIntRangeArray(0, to: 7, addZeros: false), ["."], createIntRangeArray(0, to: 10, addZeros: false), createIntRangeArray(0, to: 10, addZeros: false), createIntRangeArray(0, to: 10, addZeros: false)]
+    case "decathlon":
+        return [createIntRangeArray(0, to: 10, addZeros: false), ["."], createIntRangeArray(0, to: 10, addZeros: false), createIntRangeArray(0, to: 10, addZeros: false), createIntRangeArray(0, to: 10, addZeros: false)]
+    default:
+        return [[EMPTY_STATE]]
+    }
+    
+}
+
+// MARK:- Connections related
+func initConnectionMsgInNavigationPrompt(navigationItem: UINavigationItem) {
+    let status = Reach().connectionStatus()
+    switch status {
+    case .Unknown, .Offline:
+        log("Not connected")
+        navigationItem.prompt = "You are offline"
+    case .Online(.WWAN):
+        log("Connected via WWAN")
+        clearInformMessageForConnection(navigationItem)
+    case .Online(.WiFi):
+        log("Connected via WiFi")
+        clearInformMessageForConnection(navigationItem)
+    }
+}
+
+func clearInformMessageForConnection(navigationItem: UINavigationItem) {
+    navigationItem.prompt = nil
+}
+
+// MARK:- Logging
+func log(logMessage: String, functionName: String = __FUNCTION__, lineNum: Int = __LINE__) {
+    print("\(NSDate()) : [\(functionName)] \(logMessage) : \(lineNum)")
+}
